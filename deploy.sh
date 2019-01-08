bf560484e3fb4939598674ad4f0a63c9c2f841dd
oott123 <git@public.oott123.com>
2019-01-08 13:28:45 +0800

    兼容 0.10.0

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# install packages
+./plum/rime-install luna-pinyin
+./plum/rime-install essay
+./plum/rime-install stroke
+./plum/rime-install rime/rime-emoji@schema
+./plum/rime-install rime/rime-emoji@master
+
 # linking rime config files
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $1 $DIR/Rime/* ~/Library/Rime
